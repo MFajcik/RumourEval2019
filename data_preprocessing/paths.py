207e672e156bf1ffd579cd6035ce586ca1b57c8f
Martin Fajčík <ifajcik@fit.vutbr.cz>
2020-01-16 13:36:12 +0100

    Added comments

--- data_preprocessing/paths.py
+++ data_preprocessing/paths.py
@@ -1,5 +1,18 @@
+# TRAIN_DATA_PREFIX - folder that contains:
+#dev-key.json  reddit-dev-data  reddit-training-data  train-key.json  twitter-english
 TRAIN_DATA_PREFIX = "/home/ifajcik/Work/NLP/semeval_2019/7_Rumour_Eval/rumoureval-2019-training-data"
+
+# PATH_TO_TEST_REDDIT - folder that contains
+# 1d7lzf  1x8zke  3xqilx  4ycnf5  5tk0zx  5yp0yt  6m1mwg  7hrc7n  879j1c  8lkwd7  8m9y0o  ayn70  xn2bn
+# 1uniwn  3e7pye  4eo2zi  53czp3  5vvv51  6jk9iq  7g16as  7imq99  8e86kt  8lwt83  938o5h  wfajw
+
 PATH_TO_TEST_REDDIT= "/home/ifajcik/Work/NLP/semeval_2019/7_Rumour_Eval/rumoureval-2019-test-data/reddit-test-data"
+
+# PATH_TO_TEST_TWITTER - folder that contains
+# afghanistan       blm-hurricane    cnn-lies           harvey-donations      lawn-tornado     obama-harvey     shark-hurricane
+# african-american  cape-tornadoes   fire-ants          immigrants-wildfires  lightning-river  osteen-yacht     texas-mosque
+# ark-park          clinton-ads      fukushima-dump     inflatable-dam        mississippi-dry  pence-sacrifice  virgin-weapons
+# barbara-bush      clinton-florida  fukushima-falling  irma-cat-6            nat-geo-footage  red-cross        wildfires-deduction
 PATH_TO_TEST_TWITTER = "/home/ifajcik/Work/NLP/semeval_2019/7_Rumour_Eval/rumoureval-2019-test-data/twitter-en-test-data"
 
 # You do not need to set this if you are not using W2V features
